b7c8362ea6a9bd05241079195294f527af975f5a
lohithpuvvala <lohithpuvvala@gmail.com>
2025-03-28 16:49:16 +0530

    Updated JS file. Created a async funct to fetch random person data from ext API. Created Add User Eventlisteners and functionality

---
+++ script.js
@@ -0,0 +1,51 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let userData = [];
+
+//Fetch Random User and Add Money.
+async function getRandomUser()
+{
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+
+    const user = data.results[0];
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random()*10**6)
+    };
+    
+    addData(newUser);
+}
+
+function addData(newUser){
+    userData.push(newUser);
+    updateDOM();
+}
+
+//updateDOM
+function updateDOM(providedData = userData){
+    //Clear main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+    providedData.forEach(user => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${user.name}</strong> ${formatMoney(user.money)}`;
+
+        main.appendChild(element);
+    });
+}
+
+//format number as money
+function formatMoney(number)
+{
+    return  "$ "+(number).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+//Event Listeners
+addUserBtn.addEventListener('click',getRandomUser);
